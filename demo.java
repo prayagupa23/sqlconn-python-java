4622c7c1fd91419dd16a8cf04ee05c52b176325a
Prayag <prayagupadhyaya23@gmail.com>
2025-06-03 16:41:33 +0530

    Initial commit

---
+++ demo.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+class demo{
+    public static void main(String args[]){
+        String url = "jdbc:mysql://localhost:3306/experiment3";
+        String user= "root";
+        String password= "Prayag@2308";
+        String query = "INSERT INTO student(s_id,name,age,email) VALUES (2,'Pramod',21,'prmd@email')";
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection c= DriverManager.getConnection(url,user,password);
+            Statement s= c.createStatement();
+            int count= s.executeUpdate(query);
+            System.out.println("Number of rows affected: "+count);
+            s.close();
+            c.close();
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("Driver not found");
+        }
+        catch(SQLException e){
+            System.out.println("SQL Exception Found");
+        }
+    }
+}
